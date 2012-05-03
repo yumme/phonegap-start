47da86bbc55b9c4cdd6b84f968049db0e5a0e68a
Oscar Lagercrantz <oscar@theoffice.se>
2012-05-03 20:42:44 +0200

    Added deploy routine for Github

---
+++ fabfile.py
@@ -0,0 +1,26 @@
+import sys
+from fabric.api import *
+
+@task
+def deploy():
+  #test()
+  push()
+
+@task
+def test():
+  with lcd('/Users/Oscar/Sites/phonegap-start/'):
+    local('make test')
+
+@task
+def push():
+  with lcd('/Users/Oscar/Sites/phonegap-start/'):
+    with settings(warn_only=True):
+      local('git add .')
+      local('git status')
+      message = raw_input('Enter a git commit message: ')
+      commit = local('git commit -a -m "%s"' % message)
+      if commit.failed:
+        print 'Nothing to commit, exiting...'
+        sys.exit(0)
+      else:
+        local('git push origin master')
